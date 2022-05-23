27e1edfcfb0b3b969ca208a936edbff6e6feb0a2
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-23 19:51:21 +0500

    literals and comments

---
+++ index.js
@@ -0,0 +1,16 @@
+//Comments
+
+// this is a single-line comment
+/* This is a 
+multi-line comment*/
+
+
+//Literals
+
+12 // number
+1.2 // also number
+'humoyun' // string
+true // boolen
+false // also boolen
+null // Absence of an object
+
