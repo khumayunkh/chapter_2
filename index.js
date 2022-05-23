2caed96499a42c7d6abc741d3a143adbd1c5005a
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-23 22:06:39 +0500

    math and string

--- index.js
+++ index.js
@@ -25,9 +25,40 @@ function or class */
 
 //Арифметические действия в JavaScript:
 
-Math.pow(2, 43) // 2 в степени 43
-Math.round(.6)  // 1.0 округляет до ближайшего целого
-Math.cail(.6)  // 1.0 округляет в большую сторону до целого
-Math.floor(.6) // 0.0 округляет в меньшую сторону до целого
-console.log(Math.max(4,5,3)) // 5
+// Math.pow(2, 43) // 2 в степени 43
+// Math.round(.6)  // 1.0 округляет до ближайшего целого
+// Math.cail(.6)  // 1.0 округляет в большую сторону до целого
+// Math.floor(.6) // 0.0 округляет в меньшую сторону до целого
+// console.log(Math.max(4,5,3)) // 5
 Math.min(4,3,2) // 2
+
+
+// let x = .3 - .2 // equel 3/10 - 2/10
+// let y = .2 - .1 // equel 2/10 - 1/10 
+// console.log(x)
+// x === y  // false
+
+// x === .1 // false
+// y === .1 // true
+
+
+2 > 1n //true
+1 < 2n //true
+0 == 0n //true
+0 === 0n //false
+
+
+
+let now = new Date() // Текущее время
+console.log(now)
+let ms = now.getTime()
+console.log(ms)
+
+let human = 'Humoyun' //string
+console.log(human.length) //7
+
+
+let x = '3'
+let y = '2'
+
+x + y // 32
