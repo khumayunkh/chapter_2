9ec94507c533f5c0a719629549f149155ed7b95f
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-23 21:42:27 +0500

    math

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ null // Absence of an object
 /* reserved words must not be used as the manes of const, var, 
 function or class */
 
+// Числовые литералы с плавающей точкой:
+
+3.14e12 // equel to 3.14 * 10(23)
+3.192e-32 // equel to 3.192 * 10(-32)
+
+//Арифметические действия в JavaScript:
+
+Math.pow(2, 43) // 2 в степени 43
+Math.round(.6)  // 1.0 округляет до ближайшего целого
+Math.cail(.6)  // 1.0 округляет в большую сторону до целого
+Math.floor(.6) // 0.0 округляет в меньшую сторону до целого
+console.log(Math.max(4,5,3)) // 5
+Math.min(4,3,2) // 2
